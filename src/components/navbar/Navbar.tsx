9b8e0e9937f1e5df0838ad3c0a8a1d5a2534e201
Matthew Krain <matthewjkrain@gmail.com>
2023-02-05 16:43:24 -0500

    Home Page Update

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     }
   };
   return (
-    <div className='absolute'>
+    <div className='absolute z-50'>
       <div className='flex flex-row w-screen justify-around py-2 bg-gray-800/50 shadow-lg'>
         <button
           onClick={() => handleClickScroll("sec1")}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/navbar/Navbar";
 import Section from "@/components/Section";
+import { useState } from "react";
+import { BiDownArrow, BiUpArrow } from "react-icons/bi";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [isAboutMeOpen, setIsAboutMeOpen] = useState(false);
   return (
     <>
       <Head>
@@ -18,14 +20,74 @@ export default function Home() {
       </Head>
       <main className='snap-y snap-promixity h-screen w-screen overflow-scroll scrollbar-hide'>
         <Navbar />
+        <Section id='sec1' color='bg-slate-900' size='none'>
+          <div className='relative border-b-4 border-slate-900 shadow-xl'>
+            <div className='absolute w-full h-full top-[30%]'>
+              <div className='flex flex-col items-center'>
+                <div className='flex w-52 h-52 border-2 border-slate-500 rounded-full items-center justify-center shadow bg-slate-800'>
+                  <div className='relative w-full h-full rounded-full overflow-hidden'>
+                    <Image
+                      src='/profile.jpg'
+                      alt='profile image'
+                      fill
+                      priority
+                    />
+                  </div>
+                </div>
+                <h1
+                  className={`${inter.className} text-center font-bold text-4xl text-slate-300`}
+                >
+                  Matthew Krain
+                </h1>
+              </div>
+            </div>
+            <div className='absolute top-20 left-20 z-10'>
+              <button
+                className='cursor-pointer flex flex-row gap-x-1'
+                onClick={() => setIsAboutMeOpen(!isAboutMeOpen)}
+              >
+                <h2
+                  className={`${inter.className} select-none text-green-200 active:text-green-100`}
+                >
+                  About Me
+                </h2>
+                {!isAboutMeOpen ? (
+                  <BiDownArrow className='mt-1 text-green-400' />
+                ) : (
+                  <BiUpArrow className='mt-1 text-red-400' />
+                )}
+              </button>
 
-        <Section id='sec1' color='bg-slate-400'>
-          <div className='bg-slate-100 w-screen pt-20 pb-10 shadow-lg mb-4'>
+              {isAboutMeOpen && (
+                <div className='w-[50%]'>
+                  <p
+                    className={`${inter.className} text-sm text-slate-300 select-none bg-slate-800 p-2 rounded-2xl border-2 border-green-300`}
+                  >
+                    Full Stack Engineer with 3 years of experience in developing
+                    automation software and digital processes for the Chemical
+                    Manufacturing industry. Proficient in JavaScript/TypeScript
+                    with experience in React and React Native. Adept at working
+                    in fast-paced, collaborative environments and continuously
+                    seeking new knowledge, particularly in the area of Machine
+                    Learning Models. Proven ability to lead technical teams and
+                    produce software solutions that yield multi million-dollar
+                    savings for a Fortune 500 company.
+                  </p>
+                </div>
+              )}
+            </div>
+            <video className='z-0' autoPlay loop muted>
+              <source src='/intro2.mp4' />
+            </video>
+          </div>
+          {/*
+          <div className='bg-slate-800 w-screen pt-20 pb-10 shadow-lg'>
+            
             <div className='m-auto flex w-24 h-24 border-2 rounded-full items-center justify-center shadow'>
-              <p className='text-center text-slate-400'>Profile</p>
+              <p className='text-center text-slate-200'>Profile</p>
             </div>
             <h1
-              className={`${inter.className} text-center font-bold text-4xl text-slate-400 select-all`}
+              className={`${inter.className} text-center font-bold text-4xl text-slate-300 select-all`}
             >
               Matthew Krain
             </h1>
@@ -41,7 +103,7 @@ export default function Home() {
 
           <div className='flex flex-col w-[80%] justify-evenly gap-y-20 p-4'>
             <div className=''>
-              <h2>About Me</h2>
+              <h2 className={`${inter.className}`}>About Me</h2>
               <p className={`${inter.className} text-md text-slate-200`}>
                 Full Stack Engineer with 3 years of experience in developing
                 automation software and digital processes for the Chemical
@@ -51,47 +113,82 @@ export default function Home() {
                 new knowledge, particularly in the area of Machine Learning
                 Models. Proven ability to lead technical teams and produce
                 software solutions that yield multi million-dollar savings for a
-                Fortune 500 company
+                Fortune 500 company.
               </p>
             </div>
-            <div>
-              <h2 className=''>Skills</h2> <p>...</p>
-            </div>
-          </div>
+            <div className={`${inter.className}`}>
+              <h2>Skills</h2>
+              <div className='flex flex-row gap-x-2'>
+                <h3
+                  className='text-slate-200 underline decoration-2
+                '
+                >
+                  Languages ::
+                </h3>
+                <p> JavaScript, TypeScript, HTML, CSS, Python, C#, VBA</p>
+              </div>
+              <div className='flex flex-row gap-x-2'>
+                <h3 className='text-slate-200 underline decoration-2'>
+                  Framework/Libraries ::
+                </h3>
+                <p> React, Node.js, Express, TailwindCSS, SST, Jest</p>
+              </div>
+              <div className='flex flex-row gap-x-2'>
+                <h3 className='text-slate-200 underline decoration-2'>
+                  Leadership ::
+                </h3>
+                <p>
+                  SAP & Production Team Leader, C.I. Site Leader for Safety and
+                  Sustainability, Training Developer/Coordinator
+                </p>
+              </div>
+              <div className='flex flex-row gap-x-2'>
+                <h3 className='text-slate-200 underline decoration-2'>
+                  Communication ::
+                </h3>
+                <p>
+                  Ecolab Global Sustainability Presenter, Site Safety Trainer
+                </p>
+              </div>
+           </div>
+          </div>*/}
         </Section>
 
-        <Section id='sec2' color='bg-yellow-200'>
-          <h1 className={`${inter.className} text-2xl text-slate-500`}>Blog</h1>
+        <Section id='sec2' color='bg-sky-200'>
+          <div className='bg-slate-900 w-screen pt-20 pb-10 shadow-lg mb-4'>
+            <h1
+              className={`${inter.className} text-center font-bold text-4xl text-slate-300`}
+            >
+              Blog
+            </h1>
+          </div>
         </Section>
         <Section id='sec3' color='bg-blue-200'>
-          <h1 className={`${inter.className} text-2xl text-slate-500`}>
-            Projects
-          </h1>
+          <div className='bg-slate-900 w-screen pt-20 pb-10 shadow-lg'>
+            <h1
+              className={`${inter.className} text-center font-bold text-4xl text-slate-300`}
+            >
+              Projects
+            </h1>
+          </div>
+          <div className='w-full h-full'>
+            <video autoPlay loop muted>
+              <source src='/sand.mp4' />
+            </video>
+          </div>
         </Section>
         <Section id='sec4' color='bg-red-200'>
-          <h1 className={`${inter.className} text-2xl text-slate-500`}>
-            Contact Me
-          </h1>
-        </Section>
-        <div className={styles.description}>
-          <div>
-            <a
-              href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-              target='_blank'
-              rel='noopener noreferrer'
+          <div className='bg-slate-900 w-screen pt-20 pb-10 shadow-lg mb-'>
+            <h1
+              className={`${inter.className} text-center font-bold text-4xl text-slate-300`}
             >
-              By{" "}
-              <Image
-                src='/vercel.svg'
-                alt='Vercel Logo'
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+              Contact Me
+            </h1>
           </div>
-        </div>
+          <div className='w-full h-full relative'>
+            <Image src='/laptopRGB.jpg' alt='laptop' fill priority />
+          </div>
+        </Section>
       </main>
     </>
   );
