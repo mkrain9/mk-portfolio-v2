e19bde43f36dba6fb1ff133edbd016f5db794253
Matthew Krain <matthewjkrain@gmail.com>
2023-03-01 06:52:13 -0500

    Temp fix for _app.tsx layout type error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import Navbar from "@/components/navbar/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: {
+  Component: any;
+  pageProps: any;
+}) {
   const Layout = Component.Layout || EmptyLayout;
   return (
     <>
